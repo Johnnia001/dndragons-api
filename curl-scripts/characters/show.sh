6086c710cbc1bb22c152adad349303a6dc2b829d
johnnia001 <jarroyo.staff@gmail.com>
2022-03-06 22:20:32 -0700

    make sure api communicate with client

---
+++ curl-scripts/characters/show.sh
@@ -0,0 +1,19 @@
+curl "http://localhost:8000/characters/" \
+  --include \
+  --request GET \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token ${TOKEN}" \
+  --data '{
+    "character": {
+      "name": "'"${NAME}"'",
+      "level": "'"${LEVEL}"'",
+      "exp": "'"${EXP}"'",
+      "charClass": "'"${CHARCLASS}"'",
+      "race": "'"${RACE}"'",
+      "alignment": "'"${ALIGNMENT}"'",
+      "background": "'"${BACKGROUND}"'"
+
+    }
+  }'
+
+echo
